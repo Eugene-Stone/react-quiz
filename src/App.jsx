a11a0779d8d55b15d73b8609cd193c9ff7cd30e2
Eugene-Stone <www.kraiser@gmail.com>
2026-02-11 12:27:39 +0200

    Update

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Fragment } from 'react';
+
+import Quiz from './components/Quiz/Quiz.jsx';
+
+function App() {
+	return (
+		<Fragment>
+			<Quiz />
+		</Fragment>
+	);
+}
+
+export default App;
